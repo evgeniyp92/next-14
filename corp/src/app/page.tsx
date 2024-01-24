f8c3d6d6bf34915b6b07e4e7b304ecfcea4b0958
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-01-24 17:24:21 +0100

    Add images and update home page layout

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,9 +1,13 @@
-import Link from "next/link";
+import Image from "next/image";
+import homeImg from "/public/home.jpg";
 
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
+      <div className='absolute -z-10 inset-0'>
+        <Image src={homeImg} alt='car factory' fill style={{ objectFit: "cover" }} />
+      </div>
     </div>
   );
 }
