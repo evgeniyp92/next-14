790abf3c8a9643aa1ee3c1911afd4d73a65588cb
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-01-24 17:00:59 +0100

    Move navigation links to layout.tsx

--- corp/src/app/layout.tsx
+++ corp/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <div>
+          <Link href='/'>Home</Link>
+          <Link href='/performance'>Performance</Link>
+          <Link href='/reliability'>Reliability</Link>
+          <Link href='/scale'>Scale</Link>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
