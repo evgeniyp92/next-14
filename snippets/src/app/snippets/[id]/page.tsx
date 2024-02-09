9fd8baf90b9105275d04465ad3a4349be138705c
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-02-09 15:37:30 +0100

    Add SnippetView component

---
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface SnippetShowPageProps {
+  params: { id: string };
+}
+
+const SnippetView = async (props: SnippetShowPageProps) => {
+  const snippet = await db.snippet.findFirst({
+    where: {
+      id: parseInt(props.params.id),
+    },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>{snippet.title}</div>;
+};
+
+export default SnippetView;
