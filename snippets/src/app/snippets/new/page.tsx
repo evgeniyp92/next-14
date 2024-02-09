ca660cbeefe6e697d696457d69d6cd33eab2d039
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-02-09 14:48:35 +0100

    Add createSnippet function and form action

--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { db } from "@/db";
+import { redirect } from "next/navigation";
 
 const SnippetCreatePage = () => {
+  async function createSnippet(formData: FormData) {
+    "use server"; // This tells Next to run this code on the server
+
+    // Check the users inputs and make sure they're valid
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    // Create new record in the database
+    const createdSnippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(createdSnippet);
+
+    // Redirect the user to the home page for now (snippet page for later)
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className='font-bold m-3'>Create a Snippet</h3>
       <div className='flex flex-col gap-4'>
         <div className='flex gap-4'>
