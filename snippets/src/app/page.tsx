5a2be8e5b7bc05f661828e04f272c030d650b2e3
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-02-09 15:59:15 +0100

    Add links to snippet details and new snippet form

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { db } from "@/db";
 
 export default async function Home() {
@@ -5,8 +6,26 @@ export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map(snippet => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        href={`/snippets/${snippet.id}`}
+        className='flex justify-between items-center p-2 border rounded border-black'
+        key={snippet.id}>
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className='flex m-2 justify-between items-center'>
+        <h1 className='text-xl font-bold'>Snippets</h1>
+        <Link href='/snippets/new' className='border p-2 rounded border-black'>
+          New
+        </Link>
+      </div>
+      <div className='flex flex-col gap-2'>{renderedSnippets}</div>
+    </div>
+  );
 }
