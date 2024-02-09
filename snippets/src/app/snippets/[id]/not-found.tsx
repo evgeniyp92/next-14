209a64b6711aa4532144f3c6f1c6ffb5136b07bb
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-02-09 15:43:36 +0100

    Add SnippetNotFound component

---
+++ snippets/src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+
+const SnippetNotFound = () => {
+  return (
+    <h1 className='text-xl bold'>
+      Sorry, but we couldn't find that particular snippet...
+    </h1>
+  );
+};
+
+export default SnippetNotFound;
