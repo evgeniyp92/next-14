05462bf786e446f8d6df7162ba6aa3a0dbd8bfae
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-02-09 10:22:50 +0100

    Refactor main page component into hero component

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
 import homeImg from "/public/home.jpg";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
-      <div className='absolute -z-10 inset-0'>
-        <Image src={homeImg} alt='car factory' fill style={{ objectFit: "cover" }} />
-      </div>
-    </div>
+    <Hero
+      imgData={homeImg}
+      imgAlt='car factory assembly line'
+      title='Professional Cloud Hosting'
+    />
   );
 }
