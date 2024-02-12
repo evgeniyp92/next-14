7c0e6ea7e7fcd6b2b31a99433d1c4dea2d7ccc49
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-02-12 15:03:10 +0100

    Refactor createSnippet function to handle errors and improve code readability

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -25,29 +25,36 @@ export async function createSnippet(
   formData: FormData
 ) {
   // Check the users inputs and make sure they're valid
-  const title = formData.get('title');
-  const code = formData.get('code');
+  try {
+    const title = formData.get('title');
+    const code = formData.get('code');
 
-  if (typeof title !== 'string' || title.length < 3) {
-    return {
-      message: 'Title must be longer',
-    };
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        message: 'Title must be longer',
+      };
+    }
+
+    if (typeof code !== 'string' || code.length < 10) {
+      return {
+        message: 'Code must be longer',
+      };
+    }
+
+    // Create new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { message: err.message };
+    } else {
+      return { message: 'Something went wrong' };
+    }
   }
-
-  if (typeof code !== 'string' || code.length < 10) {
-    return {
-      message: 'Code must be longer',
-    };
-  }
-
-  // Create new record in the database
-  const createdSnippet = await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
-
   // Redirect the user to the home page for now (snippet page for later)
-  redirect('/');
+  redirect('/'); // this actually throws a very specific error that next is looking out for, and trycatch will catch redirects and break them
 }
