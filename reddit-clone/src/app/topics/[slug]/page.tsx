1246e4863567b1ba7dc3cc8950ac994a3ecb054a
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-02-13 10:54:43 +0100

    Add new pages for topic, single post, and create new post

---
+++ reddit-clone/src/app/topics/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ShowTopicPage = () => {
+  return <div>ShowTopicPage</div>;
+};
+
+export default ShowTopicPage;
---
+++ reddit-clone/src/app/topics/[slug]/post/[postId]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ShowSinglePostPage = () => {
+  return <div>ShowSinglePost</div>;
+};
+
+export default ShowSinglePostPage;
