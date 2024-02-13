d1ecab5d4b3e0ee9758c78dc57e410f70f131a17
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-02-13 10:25:16 +0100

    checking if a user is signed in from a server component

--- reddit-clone/src/app/page.tsx
+++ reddit-clone/src/app/page.tsx
@@ -1,9 +1,19 @@
 import { Button } from '@nextui-org/react';
+import * as actions from '@/actions';
+import { auth } from '@/auth';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
-      <Button>Click me</Button>
+      <form action={actions.signIn}>
+        <Button type='submit'>Sign In</Button>
+      </form>
+      <form action={actions.signOut}>
+        <Button type='submit'>Sign Out</Button>
+      </form>
+      {session?.user && <div>{JSON.stringify(session.user, null, 2)}</div>}
     </div>
   );
 }
