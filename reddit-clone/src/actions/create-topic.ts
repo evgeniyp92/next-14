0bd20f7c8dafd723930938bec600d0f37ebe73c3
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-02-14 10:36:58 +0100

    Refactor createTopicSchema and TopicCreateForm

--- reddit-clone/src/actions/create-topic.ts
+++ reddit-clone/src/actions/create-topic.ts
@@ -3,9 +3,12 @@
 import { z } from 'zod';
 
 const createTopicSchema = z.object({
-  name: z.string().regex(/^[a-z-]+$/, {
-    message: 'Must be lowercase letters or dashes without spaces',
-  }),
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: 'Must be lowercase letters or dashes without spaces',
+    }),
   description: z.string().min(10),
 });
 
